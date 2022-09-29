d79f9842834e8f8bae3396b182d291544e3cbc04
InMindVR-Dev <109471520+InMindVR-Dev@users.noreply.github.com>
2022-09-29 21:16:23 +0200

    New Timer displayer simple behaviour

--- UnityClasses/Behaviours/GetPointingDataBehaviourExample.cs
+++ UnityClasses/Behaviours/GetPointingDataBehaviourExample.cs
@@ -1,10 +1,5 @@
-﻿using HumanVirtualMaze.Behaviours;
-using HumanVirtualMaze.Model;
-using System;
-using System.Collections.Generic;
+﻿using HumanVirtualMaze.Model;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 
