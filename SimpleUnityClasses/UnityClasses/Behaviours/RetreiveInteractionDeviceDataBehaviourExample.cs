6bbc49c6ae9380ef6fb0bba1a13e9d63112c022f
InMindVR-Dev <109471520+InMindVR-Dev@users.noreply.github.com>
2023-01-05 15:27:10 +0100

    Infos update

--- SimpleUnityClasses/UnityClasses/Behaviours/RetreiveInteractionDeviceDataBehaviourExample.cs
+++ SimpleUnityClasses/UnityClasses/Behaviours/RetreiveInteractionDeviceDataBehaviourExample.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 namespace UnityClasses
 {
+
+    //This class show how to use LiveData.EventManager.InteractionData
+    //Since it can use more than 1x pointing device, InteractionData is a list
+    //Most of the time it contains only one child
     public class RetreiveInteractionDeviceDataBehaviourExample : MonoBehaviour
     {
 
@@ -13,25 +17,25 @@ namespace UnityClasses
             Console.WriteLine("Retreiving Interaction devices Data !");
             foreach (var item in LiveData.EventManager.InteractionData)
             {
-                //on a des valeurs d'axes genre joystick
+                //We can get Vector3 values (containing verctor2 sometimes)
 
                 foreach (var axe in item.Axes)
                 {
                     Console.WriteLine("Axis value : " + axe.ToString());
                 }
 
-                //On peut avoir des valeurs analogiques genre gachette
+                //We can get float values
 
                 foreach (var analog in item.Analogs)
                 {
                     Console.WriteLine("Analog value : " + analog.ToString());
                 }
 
-                //On peut avoir des valeurs booleennes genre boutons
+                //We can get bool values
 
                 foreach (var digital in item.Digitals)
                 {
-                    Console.WriteLine("Analog value : " + digital.ToString());
+                    Console.WriteLine("digital value : " + digital.ToString());
                 }
 
             }
